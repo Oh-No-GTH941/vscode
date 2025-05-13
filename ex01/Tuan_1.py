bffe54544af0451f472ce8264bf22371b9aaabfc
Oh-No-GTH941 <giaptronghieu7915@gmail.com>
2025-05-13 17:03:11 +0700

    xin chao vs code

---
+++ ex01/Tuan_1.py
@@ -0,0 +1,14 @@
+print ("hello , xin chao ")
+
+X = int (input("Nhập X là : "))
+Y = int (input("Nhập X là : "))
+
+cong = X + Y 
+tru = X - Y
+nhan = X * Y 
+chia = X / Y 
+
+print ("Kết quả + : ",cong)
+print ("Kết quả - : ",tru)
+print ("Kết quả * : ", nhan)
+print ("Kết quả / : ", chia)
